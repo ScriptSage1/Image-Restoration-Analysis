fd74c51fd957a0e288872ffc205185a58c5804e0
Shaik Riyaz <shaikriyaz9800@gmail.com>
2025-10-26 18:01:03 +0530

    made adjustments to the close up images

--- distortions/__init__.py
+++ distortions/__init__.py
@@ -3,13 +3,12 @@ from .color_shift import apply_ColorShift
 from .gaussian_noise import apply_GaussianNoise
 from .jpeg_compression import apply_JpegCompression
 from .down_sampling import apply_DownSampling
-from .visualizer import visualize_comparison
+
 
 __all__ = [
     "apply_GaussianBlur",
     "apply_ColorShift",
     "apply_JpegCompression",
     "apply_DownSampling",
-    "apply_GaussianNoise",
-    "visualize_comparison"
+    "apply_GaussianNoise"
 ]
