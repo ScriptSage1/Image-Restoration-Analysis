9d65e251cfc007eafaad69470ec875c0669b1ea2
Shaik Riyaz <shaikriyaz9800@gmail.com>
2025-10-29 02:00:46 +0530

    added notebook for DownSampling Analysis

--- distortions/down_sampling.py
+++ distortions/down_sampling.py
@@ -1,13 +1,19 @@
+# In distortions/down_sampling.py
+
 import cv2 as cv
 
-def apply_DownSampling(image,scale_factor=0.5):
-
-  h, w = image.shape[:2]
-
-  new_h , new_w = int(h*scale_factor), int (w*scale_factor)
-
-  image_down = cv.resize(image,(new_w,new_h),interpolation = cv.INTER_AREA)
-
-  image_up = cv.resize(image_down, (w, h), interpolation = cv.INTER_LINEAR)
-
-  return image_up
+def apply_DownSampling(image, scale_factor=2):
+    """
+    Creates a low-resolution image by downsampling.
+    """
+    h, w = image.shape[:2]
+    
+    # Calculate new dimensions
+    new_h = h // scale_factor
+    new_w = w // scale_factor
+    
+    # Create the low-resolution image
+    image_down = cv.resize(image, (new_w, new_h), interpolation=cv.INTER_AREA)
+    
+    # Return ONLY the small H/2, W/2 image
+    return image_down
